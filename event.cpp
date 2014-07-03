9f043d2043250998f0d9bfcf6afb253e25fc5e54
MrVil <vilghost@gmail.com>
2014-07-03 10:29:26 +0200

    Separation du event handler, non fonctionnel

---
+++ event.cpp
@@ -0,0 +1,12 @@
+#include "event.hpp"
+
+void mainEventHandler(sf::Window window){
+
+	    sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+}
---
+++ event.hpp
@@ -0,0 +1,12 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+/* README :
+ * Differents event handlers.
+ */
+
+#include <SFML/Graphics.hpp>
+
+void mainEventHandler(sf::Window);
+
+#endif /*EVENT_HPP*/
